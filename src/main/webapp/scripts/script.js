ff2023dc82efdc65008362ddb7a6bbe1603f0bf7
vignesh-4641 <vignesh-4641@localhost>
2019-08-03 17:10:52 +0530

    added js for reference

---
+++ src/main/webapp/scripts/script.js
@@ -0,0 +1,77 @@
+/**
+ * @name handleFail
+ * @param err - error thrown by any function
+ * @description Helper function to handle errors
+ */
+let handleFail = function(err){
+    console.log("Error : ", err);
+};
+
+// Queries the container in which the remote feeds belong
+let remoteContainer= document.getElementById("remote-container");
+
+/**
+ * @name addVideoStream
+ * @param streamId
+ * @description Helper function to add the video stream to "remote-container"
+ */
+function addVideoStream(streamId){
+    let streamDiv=document.createElement("div"); // Create a new div for every stream
+    streamDiv.id=streamId;                       // Assigning id to div
+    streamDiv.style.transform="rotateY(180deg)"; // Takes care of lateral inversion (mirror image)
+    remoteContainer.appendChild(streamDiv);      // Add new div to container
+}
+/**
+ * @name removeVideoStream
+ * @param evt - Remove event
+ * @description Helper function to remove the video stream from "remote-container"
+ */
+function removeVideoStream (evt) {
+    let stream = evt.stream;
+    stream.stop();
+    let remDiv=document.getElementById(stream.getId());
+    remDiv.parentNode.removeChild(remDiv);
+    console.log("Remote stream is removed " + stream.getId());
+}
+
+let client = AgoraRTC.createClient({
+    mode: 'live',
+    codec: "h264"
+});
+client.init("a6c165dd918b4845810d406634526ccd",() => console.log("AgoraRTC client initialized") ,handleFail);
+
+client.join(null,"dsc-video-demo",null, (uid)=>{
+
+    // Stream object associated with your web cam is initialized
+    let localStream = AgoraRTC.createStream({
+        streamID: uid,
+        audio: true,
+        video: true,
+        screen: false
+    });
+
+    // Associates the stream to the client
+    localStream.init(function() {
+
+        //Plays the localVideo
+        localStream.play('me');
+
+        //Publishes the stream to the channel
+        client.publish(localStream, handleFail);
+
+    },handleFail);
+
+},handleFail);
+
+client.on('stream-added', function (evt) {
+    client.subscribe(evt.stream, handleFail);
+});
+//When you subscribe to a stream
+client.on('stream-subscribed', function (evt) {
+    let stream = evt.stream;
+    addVideoStream(stream.getId());
+    stream.play(stream.getId());
+});
+//When a person is removed from the stream
+client.on('stream-removed',removeVideoStream);
+client.on('peer-leave',removeVideoStream);
